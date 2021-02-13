9baa866d68de3b883bd12bd2a6178a814fc73e0c
Jose <joseeduardocorella@gmail.com>
2021-02-13 10:48:19 -0800

    Initial Commit

---
+++ iPad-Go/iPad_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPad_GoApp.swift
+//  iPad-Go
+//
+//  Created by Jose Corella on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct iPad_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
